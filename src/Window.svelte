36d0dcf2743186f0fc70ee03395f1e2441b76a19
mhj2281@naver.com <mhj2281@naver.com>
2023-10-15 15:08:54 +0900

    네비게이션 UI 제작

---
+++ src/Window.svelte
@@ -0,0 +1,32 @@
+<!-- Fonts -->
+<link
+rel="stylesheet"
+href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+<link
+rel="stylesheet"
+href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+/>
+
+<!-- Material Typography -->
+<link
+rel="stylesheet"
+href="https://unpkg.com/@material/typography@14.0.0/dist/mdc.typography.css"
+/>
+
+<!-- SMUI -->
+<link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+
+<script lang="ts">
+	import svelteLogo from './assets/svelte.svg';
+	import viteLogo from '/vite.svg';
+	import Counter from './libs/Counter.svelte';
+	import Tabbar from './libs/Tabbar.svelte';
+</script>
+
+
+<main>
+	<div>
+		<Tabbar/>
+	</div>
+</main>
---
+++ src/libs/Tabbar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import Tab, { Icon, Label } from '@smui/tab';
+  	import TabBar from '@smui/tab-bar';
+    type TabEntry = {
+     k: number;
+     icon: string;
+     label: string;
+    };
+    const key = (tab: TabEntry) => tab.k;
+    let tabs: TabEntry[] = [
+   	{
+		 k: 1,
+	 	icon: '',
+	 	label: '커피',
+   	},
+   	{
+		 k: 2,
+		 icon: '',
+		 label: '에이드',
+   	},
+   	{
+	 	k: 3,
+	 	icon: '',
+	 	label: '스무디',
+   	},
+   	{
+		 k: 4,
+		 icon: '',
+		 label: '디저트',
+ 	  },
+ 	];
+ 	let active = tabs[2];
+</script>
+
+
+<div class="fixed left-0 top-0 h-full w-full overflow-y-hidden flex flex-col items-start p-4 ">
+	<TabBar {tabs} let:tab {key} bind:active class="bg-blue-500">
+		<Tab
+			{tab}
+			stacked={true}
+			indicatorSpanOnlyContent={true}
+			tabIndicator$transition="fade"
+		>
+			<Label class="text-xl font-bold text-black">{tab.label}</Label>
+		</Tab>
+	</TabBar>
+</div>
